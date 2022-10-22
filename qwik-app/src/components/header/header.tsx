90e7aab2ec2b025d6af1775ba127b0aaf8b3b0c7
LVOL98 <lukas98@live.dk>
2022-10-22 21:13:30 +0200

    empty setup done

--- qwik-app/src/components/header/header.tsx
+++ qwik-app/src/components/header/header.tsx
@@ -8,24 +8,24 @@ export default component$(() => {
   return (
     <header>
       <div class="logo">
-        <a href="https://qwik.builder.io/" target="_blank">
+        {/* <a href="https://qwik.builder.io/" target="_blank">
           <QwikLogo />
-        </a>
+        </a> */}
       </div>
       <ul>
         <li>
-          <a href="https://qwik.builder.io/docs/components/overview/" target="_blank">
-            Docs
+          <a>
+            CV
           </a>
         </li>
         <li>
-          <a href="https://qwik.builder.io/examples/introduction/hello-world/" target="_blank">
-            Examples
+          <a>
+            Programming
           </a>
         </li>
         <li>
-          <a href="https://qwik.builder.io/tutorial/welcome/overview/" target="_blank">
-            Tutorials
+          <a>
+            Martial Arts
           </a>
         </li>
       </ul>
