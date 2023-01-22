ce4382cc7c8032552c72e4d6bf9060c72cf6c719
Lukas Vinther Offenberg Larsen <lukas98@live.dk>
2023-01-22 16:51:13 +0100

    finalized top menu var

--- qwik-app/src/components/header/header.tsx
+++ qwik-app/src/components/header/header.tsx
@@ -1,13 +1,24 @@
-import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import { component$, useSignal, useStylesScoped$ } from '@builder.io/qwik';
 import { Link } from '@builder.io/qwik-city';
 import { Paths } from '~/shared/paths';
 import styles from './header.css?inline';
 
 export default component$(() => {
   useStylesScoped$(styles);
+  const headerId = 'lvol-header'
 
   return (
-    <header>
+    <header id={headerId} 
+    window:onScroll$={(e) => {
+      const currentScrollValue = window.scrollY;
+      // @ts-ignore - target is the document 
+      const header = e.target?.getElementById(headerId);
+      if (currentScrollValue !== 0) {
+        header.classList.add('hidden');
+      } else {
+        header.classList.remove('hidden');
+      }
+    }}>
       <ul>
         <li>
           <a href={Paths.Home} class='welp' >
