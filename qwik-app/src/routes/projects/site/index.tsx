86fc316567e93779b8598d346d69c5408a79fea7
Lukas Vinther Offenberg Larsen <lukas98@live.dk>
2023-02-26 09:49:49 +0100

    Updated this site project

--- qwik-app/src/routes/projects/site/index.tsx
+++ qwik-app/src/routes/projects/site/index.tsx
@@ -1,23 +1,27 @@
 import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { DocumentHead, Link } from '@builder.io/qwik-city';
 import ProjectInfoSite from '~/components/project_info_site/project-info-site';
+import { Paths } from '~/shared/paths';
 
 export default component$(() => {
   let keywords = ['Qwik', 'NodeJS', 'Github Pages', 'Github Actions']
 
   return (
     <section>
-      <ProjectInfoSite 
+      <ProjectInfoSite
         logoPath='/images/qwik/qwik-logo.png'
         title='A Personal Website, With Qwik'
         imagePaths={['']}
         keywords={keywords}
       >
         <p>
-          When deciding to make my personal website, I wanted to use a technology that was new and in a "framework family" I never had used before!
+          Creating my personal website was an opportunity to explore new technologies in a framework that I hadn't used before. While I was familiar with frameworks like <Link href='https://dotnet.microsoft.com/en-us/'>.NET</Link> and <Link href='https://flutter.dev/'>Flutter</Link>, I wanted to see what else was out there.
         </p>
         <p>
-          Having never used a React-like framework, I decided to see if that "framework family" had any interesting thing to offer, where I found Qwik a "HTML first framework" offering fast time to interactivity
+          After some research, I discovered <Link href='https://qwik.builder.io/'>Qwik</Link>, an "HTML first framework" that offers a fast time to interactivity. As someone who had never used a React-like framework before, I was intrigued by what <Link href='https://qwik.builder.io/'>Qwik</Link> had to offer. <Link href='https://qwik.builder.io/'>Qwik</Link> also allowed me to dive deeper into website performance optimization, which will help me in the future.
+        </p>
+        <p>
+          Feel free to take a look around my website, and if you have any feedback or would like to get in touch, you can find my <Link href={Paths.Contact}>contact information</Link> on the <Link href={Paths.Contact}>contact page</Link>.
         </p>
       </ProjectInfoSite>
     </section>
