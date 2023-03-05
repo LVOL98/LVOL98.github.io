4f27cb3a650271a7b023dde91147646515004405
LVOL98 <lukas98@live.dk>
2023-03-05 17:11:06 +0100

    Removed qwik project

--- qwik-app/src/shared/paths.tsx
+++
@@ -1,8 +0,0 @@
-export const enum Paths {
-  Home = "/",
-  Site = "/projects/site",
-  HikersApp = "/projects/hikers_app",
-  Projects = "/projects",
-  Contact = "/contact",
-  IntraShare = "/projects/intrashare",
-}
